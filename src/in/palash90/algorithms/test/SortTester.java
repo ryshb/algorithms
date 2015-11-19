fd10e48cfb2bd5eae90841812eb7632712f31c66
Palash Kundu <palash.k@HCL.COM>
2015-11-19 13:02:08 +0530

    Added timestamp in the console

--- src/in/palash90/algorithms/test/SortTester.java
+++ src/in/palash90/algorithms/test/SortTester.java
@@ -19,6 +19,7 @@ package in.palash90.algorithms.test;
 import in.palash90.algorithms.sort.InsertionSort;
 import in.palash90.algorithms.sort.MergeSort;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -35,25 +36,30 @@ public class SortTester {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int length = 50000;
+		int length = 50000000;
 		int[] original = new int[length];
 
 		for (int i = 0; i < length; i++) {
-			// original[i] = (int) (length * Math.random());
-			original[i] = length - i;
+			original[i] = (int) (length * Math.random());
+			// original[i] = i;
 		}
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss.SSS");
 		MergeSort mergeSort = new MergeSort();
 		Date d1 = new Date();
+		System.out.println("Merge sort Started at: " + sdf.format(d1));
 		mergeSort.sort(original);
 		Date d2 = new Date();
+		System.out.println("Merge sort Completed at: " + sdf.format(d2));
 		System.out
 				.printf("Time taken to sort %d element using Merge Sort: %d miliseconds\n",
 						length, (d2.getTime() - d1.getTime()));
 
 		InsertionSort insertionSort = new InsertionSort();
 		d1 = new Date();
+		System.out.println("Insertion sort Started at: " + sdf.format(d1));
 		insertionSort.sort(original);
 		d2 = new Date();
+		System.out.println("Insertion sort Completed at: " + sdf.format(d2));
 		System.out
 				.printf("Time taken to sort %d element using Insertion Sort: %d miliseconds",
 						length, (d2.getTime() - d1.getTime()));
